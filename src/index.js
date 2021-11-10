2c418ff4b1bf6ad63a9b8e95ba73dc057fec4a99
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-11-10 22:48:23 +0200

    toolkit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
